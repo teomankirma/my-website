0ef1720ab81ece9a4624b4f7dd9865771b67049a
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-02-13 17:59:45 +0300

    feat: education section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,7 @@ import image from "../images/photo.jpeg";
 function AboutMe() {
   return (
     <div className="about-me section">
-      <img className="profile-pic" src={image}></img>
+      <img className="profile-pic" src={image} alt="logo"></img>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Sit amet mauris
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,9 +1,40 @@
 import PageDivider from "./PageDivider";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import firstSchool from "../images/first-school.jpeg";
+import middleSchool from "../images/middle-school.jpeg";
+import highSchool from "../images/high-school.jpeg";
+import university from "../images/university.jpeg";
 
 function Education() {
   return (
     <div className="education section">
       <PageDivider />
+      <h1 className="education-text">Education</h1>
+      <Carousel
+        infiniteLoop={true}
+        emulateTouch={true}
+        showStatus={false}
+        showThumbs={false}
+        width={600}
+      >
+        <div>
+          <img src={firstSchool} alt="first school"></img>
+          <p className="legend">Abdi Ibrahim First School</p>
+        </div>
+        <div>
+          <img src={middleSchool} alt="middle school"></img>
+          <p className="legend">Istanbul Academy College Middle School</p>
+        </div>
+        <div>
+          <img src={highSchool} alt="high school"></img>
+          <p className="legend">Emlakbank Suleyman Demirel High School</p>
+        </div>
+        <div>
+          <img src={university} alt="university"></img>
+          <p className="legend">Nisantasi University 3/4</p>
+        </div>
+      </Carousel>
     </div>
   );
 }
