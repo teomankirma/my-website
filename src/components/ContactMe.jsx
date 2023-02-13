cccc7f30e141d8c51ed1d0be25bbcf35f68aaf6e
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-02-13 20:23:19 +0300

    feat: contact me section

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,9 +1,27 @@
 import PageDivider from "./PageDivider";
+import hi from "../images/hi.gif";
 
 function ContactMe() {
   return (
     <div className="contact-me section">
       <PageDivider />
+      <h1>Contact Me</h1>
+      <img src={hi} alt="hi"></img>
+      <table className="center">
+        <tr>
+          <td>
+            <a href="https://www.linkedin.com/in/teoman-k%C4%B1rma-14ba31238/">
+              LinkedIn
+            </a>
+          </td>
+          <td>
+            <a href="https://twitter.com/teomankirma">Twitter</a>
+          </td>
+          <td>
+            <a href="mailto:teomankirma@gmail.com">Email</a>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
