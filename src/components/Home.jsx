5de852694f38352d52f5c43f8fb8369b05f86117
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-02-28 16:07:03 +0300

    feat: home section improvements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,49 +1,102 @@
 import { Typewriter } from "react-simple-typewriter";
-import { Text, Button, Spacer } from "@nextui-org/react";
+import { Text, Button, Spacer, Container, Row, Col } from "@nextui-org/react";
 
 function Home() {
   return (
-    <div className="home row section" id="home">
-      <div className="col-lg-12">
+    <div className="home section" id="home">
+      <Container>
         <Spacer y={6} />
-        <Text h2>Welcome</Text>
-      </div>
-      <div className="col-lg-12">
-        <Spacer y={1} />
-        <Text h1 weight="semibold">
-          <Typewriter
-            words={[
-              "I'm Teoman Kirma.",
-              "I'm a Software Engineer.",
-              "I'm a Frontend Developer.",
-            ]}
-            loop={0}
-            cursor={true}
-          />
-        </Text>
-      </div>
-      <div className="col-lg-12">
-        <Spacer y={1} />
-        <Text h3 weight="medium">
-          based in Izmir, Turkiye.
-        </Text>
-        <Spacer y={2} />
-      </div>
-      <div className="col-lg-12 d-flex justify-content-center">
-        <Button
-          color="success"
-          size="lg"
-          rounded
-          auto
-          ghost
-          css={{ textAlign: "center" }}
-        >
-          Hire Me
-        </Button>
-      </div>
-      <Spacer y={6} />
+        <Row>
+          <Col>
+            <Text h2>Welcome</Text>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Spacer y={1} />
+            <Text h1 weight="semibold">
+              <Typewriter
+                words={[
+                  "I'm Teoman Kirma.",
+                  "I'm a Software Engineer.",
+                  "I'm a Frontend Developer.",
+                ]}
+                loop={0}
+                cursor={true}
+                cursorColor="#17c964"
+              />
+            </Text>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Spacer y={1} />
+            <Text h3 weight="medium">
+              based in Istanbul, Turkiye.
+            </Text>
+            <Spacer y={2} />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="hire-me-btn">
+            <Button
+              color="success"
+              size="lg"
+              rounded
+              auto
+              ghost
+              css={{ zIndex: "0" }}
+            >
+              Hire Me
+            </Button>
+          </Col>
+        </Row>
+        <Spacer y={6} />
+      </Container>
     </div>
   );
 }
 
 export default Home;
+
+{
+  /* <div className="col-lg-12">
+<Spacer y={6} />
+<Text h2>Welcome</Text>
+</div>
+<div className="col-lg-12">
+<Spacer y={1} />
+<Text h1 weight="semibold">
+  <Typewriter
+    words={[
+      "I'm Teoman Kirma.",
+      "I'm a Software Engineer.",
+      "I'm a Frontend Developer.",
+    ]}
+    loop={0}
+    cursor={true}
+    cursorColor="#17c964"
+  />
+</Text>
+</div>
+<div className="col-lg-12">
+<Spacer y={1} />
+<Text h3 weight="medium">
+  based in Istanbul, Turkiye.
+</Text>
+<Spacer y={2} />
+</div>
+<div className="col-lg-12 d-flex justify-content-center">
+<Button
+  color="success"
+  size="lg"
+  rounded
+  auto
+  ghost
+  css={{ textAlign: "center" }}
+>
+  Hire Me
+</Button>
+</div>
+<Spacer y={6} /> */
+}
