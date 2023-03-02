aea0ceeb63793c7f49d8b40a636c6a8c3c67ffae
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-03-02 18:38:52 +0300

    feat: my work section clickable items

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,16 +1,90 @@
-import { Card } from '@nextui-org/react';
+import { Card, Modal, Spacer, Text } from '@nextui-org/react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function PortfolioCard(props) {
+  const [visible, setVisible] = useState(false);
+  const handler = () => setVisible(true);
+
+  const closeHandler = () => setVisible(false);
+
   return (
-    <Card>
-      <Card.Image
-        src={props.src}
-        objectFit="cover"
-        width="100%"
-        height={props.height}
-        alt={props.alt}></Card.Image>
-    </Card>
+    <div>
+      <Card isHoverable isPressable onPress={handler}>
+        <Card.Image
+          src={props.src}
+          objectFit="cover"
+          width="100%"
+          height={props.height}
+          alt={props.alt}></Card.Image>
+      </Card>
+      <Modal
+        closeButton
+        blur
+        open={visible}
+        onClose={closeHandler}
+        width={'80%'}
+        css={{ display: 'flex' }}>
+        <Modal.Header>
+          <Text h4>Project Title</Text>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row">
+            <div className="col-lg-6">
+              <Spacer y={1} />
+              <img src={props.src} />
+            </div>
+            <div className="col-lg-6 know-me-more-li">
+              <ul>
+                <li>
+                  <Text weight="medium">
+                    <b className="portfolio-li-b">Project Info:</b>
+                    <Spacer y={1} />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                  </Text>
+                </li>
+                <Spacer y={2} />
+                <li>
+                  <Text weight="medium">
+                    <b className="portfolio-li-b">Project Details:</b>
+                    <Spacer y={1} />
+                  </Text>
+                </li>
+                <li>
+                  <Text weight="medium">
+                    <b className="li-b">Client:</b>Teoman Kirma
+                  </Text>
+                  <hr className="li-hr" />
+                </li>
+                <li>
+                  <Text weight="medium">
+                    <b className="li-b">Technologies:</b>
+                  </Text>
+                  <hr className="li-hr" />
+                </li>
+                <li>
+                  <Text weight="medium">
+                    <b className="li-b">Industry:</b>
+                  </Text>
+                  <hr className="li-hr" />
+                </li>
+                <li>
+                  <Text weight="medium">
+                    <b className="li-b">Date:</b>
+                  </Text>
+                  <hr className="li-hr" />
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
   );
 }
 
