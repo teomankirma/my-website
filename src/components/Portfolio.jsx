89dde1f59b9463e796c959c40d83f635b0e6399e
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-03-04 19:24:17 +0300

    feat: portfolio styling fixes and removed filter buttons
    filtering is not working with modals. modals are not assigning to every single card component. (or i couldn't figure out :D )

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import SectionHeader from './SectionHeader';
-import { Spacer, Button } from '@nextui-org/react';
+import { Spacer } from '@nextui-org/react';
 import PortfolioCard from './PortfolioCard';
 import atm from '../images/atm.png';
 import blogWebsite from '../images/blog-website.png';
@@ -15,33 +15,27 @@ function Portfolio() {
       </div>
       <Spacer y={3} />
 
-      {/* Button Groups */}
-      <div className="row" id="buttonContainer">
-        <div className="col-lg-12 d-flex justify-content-center">
-          <Button.Group color="success" css={{ zIndex: '0' }} ghost>
-            <Button>All</Button>
-            <Button>Web</Button>
-            <Button>Other</Button>
-          </Button.Group>
-        </div>
-      </div>
-      <Spacer y={3} />
-
       {/* Portfolio Cards */}
       <div className="row">
         <div className="col-lg-4">
           <PortfolioCard src={atm} alt="atm" />
           <Spacer y={1} />
-          <PortfolioCard src={myWebsite} alt="my-website" />
         </div>
         <div className="col-lg-4">
           <PortfolioCard src={blogWebsite} alt="blog-website" />
           <Spacer y={1} />
-          <PortfolioCard src={weather} alt="weather" height={280} />
         </div>
         <div className="col-lg-4">
           <PortfolioCard src={keeper} alt="keeper" />
           <Spacer y={1} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-6">
+          <PortfolioCard src={myWebsite} alt="my-website" height={400} />
+        </div>
+        <div className="col-lg-6">
+          <PortfolioCard src={weather} alt="weather" height={400} />
         </div>
       </div>
       <Spacer y={3} />
